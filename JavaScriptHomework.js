44ce922ace3a639a6d809fc6a8da8ee72778d75d
Melina Driscoll <melina.driscoll@gmail.com>
2016-08-09 15:51:16 -0400

    javascript homework

---
+++ JavaScriptHomework.js
@@ -0,0 +1,82 @@
+// Variables
+	// x=4
+	// y=5
+
+	// console.log(x+y);
+
+
+// Debugging
+	// if(3<1){
+	// 	console.log("This is false so it does not show up");
+	// }
+	// else{
+	// 	console.log("This is true");
+	// }
+
+	// var x = 13;
+	// if(x<8){
+	// 	alert("X is less than 8");
+	// }
+	// else{
+	// 	alert("X is not less than 8")
+	// }
+
+// Different data types
+	// var user = {
+	// 	firstName: "Melina",
+	// 	lastName: "Driscoll",
+	// 	birthday: "August 20, 1999"
+	// };
+
+	// var x=20;
+	// var y=10;
+
+// Arrays
+	// favFoods = [["Pizza", "Chocolate"], ["Apples", "Chicken", "Berries"]];
+
+// Logic
+	// if(4<9){
+	// 	console.log('4 is less than 9');
+	// }
+	// else if(4===4){
+	// 	console.log("4 is always equal to 4");
+	// }
+	// else{
+	// 	console.log("won't show because 4 is less than 9");
+	// }
+
+// Functions
+	// x=34;
+
+	// while(x<39)
+	// {
+	// 	console.log("On number" + x)
+	// 	x++;
+	// }
+
+
+x=23;
+
+	while(x<29)
+	{
+		console.log("On number" + x)
+		x++;
+	}
+
+
+
+var user = {
+	birthday: "8/20/99",
+	age: "16",
+	name: "Melina Driscoll",
+	school: "Brooklyn Tech"
+};
+
+
+
+alert("My birthday is " + user.birthday + " so my age is " + user.age);
+
+
+
+console.log(user.name);
+console.log(user.school);
